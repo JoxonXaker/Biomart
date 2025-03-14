4130494af5251215ba62648efef73fe6f04361bc
JoxonXaker <joxonxaker@gmail.com>
2025-03-14 22:02:41 +0500

    some changes

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static')
 STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
 # STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage' # new
 
