98d754b15e2a2be44fd6f5483a03003e82260e24
JoxonXaker <joxonxaker@gmail.com>
2025-03-14 21:48:13 +0500

    static files change

--- config/settings.py
+++ config/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static')
-# STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
 # STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage' # new
 
 
